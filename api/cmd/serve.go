1e40e3d6e79df7980fb4d5f406f4c5a0c2bf8232
ymdarake <sakamoto.takumi.dev@gmail.com>
2024-08-10 12:43:16 +0900

    :tada: init

---
+++ api/cmd/serve.go
@@ -0,0 +1,41 @@
+/*
+Copyright © 2024 NAME HERE <EMAIL ADDRESS>
+*/
+package cmd
+
+import (
+	"net/http"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+	"github.com/spf13/cobra"
+)
+
+// serveCmd represents the serve command
+var serveCmd = &cobra.Command{
+	Use:   "serve",
+	Short: "start server",
+	Long:  `start server`,
+	Run: func(cmd *cobra.Command, args []string) {
+		r := chi.NewRouter()
+		r.Use(middleware.Logger)
+		r.Get("/", func(w http.ResponseWriter, r *http.Request) {
+			w.Write([]byte("welcome"))
+		})
+		http.ListenAndServe(":8082", r)
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(serveCmd)
+
+	// Here you will define your flags and configuration settings.
+
+	// Cobra supports Persistent Flags which will work for this command
+	// and all subcommands, e.g.:
+	// serveCmd.PersistentFlags().String("foo", "", "A help for foo")
+
+	// Cobra supports local flags which will only run when this command
+	// is called directly, e.g.:
+	// serveCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
---
+++ api/main.go
@@ -0,0 +1,10 @@
+/*
+Copyright © 2024 NAME HERE <EMAIL ADDRESS>
+*/
+package main
+
+import "keycloak-demo-5/cmd"
+
+func main() {
+	cmd.Execute()
+}
